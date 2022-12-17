24e66ea30f441bc15858b36d4a34d3a5488c7b4e
robmikh <rob.mikh@outlook.com>
2022-12-16 22:26:03 -0800

    cleanup some pinvoke

---
+++ WinUI3CaptureSample/Handle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUI3CaptureSample
+{
+    class EmptyHandle : SafeHandle
+    {
+        public EmptyHandle() : base(IntPtr.Zero, true)
+        {
+        }
+
+        public override bool IsInvalid => true;
+
+        protected override bool ReleaseHandle()
+        {
+            return true;
+        }
+    }
+}
--- WinUI3CaptureSample/MonitorEnumerationHelper.cs
+++ WinUI3CaptureSample/MonitorEnumerationHelper.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using Windows.Foundation;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+using static Windows.Win32.PInvoke;
 
 namespace WinUI3CaptureSample
 {
@@ -18,60 +21,34 @@ namespace WinUI3CaptureSample
 
     static class MonitorEnumerationHelper
     {
-        delegate bool EnumMonitorsDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int left;
-            public int top;
-            public int right;
-            public int bottom;
-        }
-
-        private const int CCHDEVICENAME = 32;
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        internal struct MonitorInfoEx
-        {
-            public int Size;
-            public RECT Monitor;
-            public RECT WorkArea;
-            public uint Flags;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
-            public string DeviceName;
-        }
-
-        [DllImport("user32.dll")]
-        static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, EnumMonitorsDelegate lpfnEnum, IntPtr dwData);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        static extern bool GetMonitorInfo(IntPtr hMonitor, ref MonitorInfoEx lpmi);
-
         public static IEnumerable<MonitorInfo> GetMonitors()
         {
             var result = new List<MonitorInfo>();
 
-            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero,
-                delegate (IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
-                {
-                    MonitorInfoEx mi = new MonitorInfoEx();
-                    mi.Size = Marshal.SizeOf(mi);
-                    bool success = GetMonitorInfo(hMonitor, ref mi);
-                    if (success)
+            unsafe
+            {
+                EnumDisplayMonitors(new EmptyHandle(), null,
+                    delegate (HMONITOR hMonitor, HDC hdcMonitor, RECT* lprcMonitor, LPARAM dwData)
                     {
-                        var info = new MonitorInfo
+                        MONITORINFOEXW win32Info = new MONITORINFOEXW();
+                        win32Info.monitorInfo.cbSize = (uint)Marshal.SizeOf(win32Info);
+                        bool success = GetMonitorInfo(hMonitor, ref win32Info.monitorInfo);
+                        if (success)
                         {
-                            ScreenSize = new Vector2(mi.Monitor.right - mi.Monitor.left, mi.Monitor.bottom - mi.Monitor.top),
-                            MonitorArea = new Rect(mi.Monitor.left, mi.Monitor.top, mi.Monitor.right - mi.Monitor.left, mi.Monitor.bottom - mi.Monitor.top),
-                            WorkArea = new Rect(mi.WorkArea.left, mi.WorkArea.top, mi.WorkArea.right - mi.WorkArea.left, mi.WorkArea.bottom - mi.WorkArea.top),
-                            IsPrimary = mi.Flags > 0,
-                            Hmon = hMonitor,
-                            DeviceName = mi.DeviceName
-                        };
-                        result.Add(info);
-                    }
-                    return true;
-                }, IntPtr.Zero);
+                            var info = new MonitorInfo
+                            {
+                                ScreenSize = new Vector2(win32Info.monitorInfo.rcMonitor.right - win32Info.monitorInfo.rcMonitor.left, win32Info.monitorInfo.rcMonitor.bottom - win32Info.monitorInfo.rcMonitor.top),
+                                MonitorArea = new Rect(win32Info.monitorInfo.rcMonitor.left, win32Info.monitorInfo.rcMonitor.top, win32Info.monitorInfo.rcMonitor.right - win32Info.monitorInfo.rcMonitor.left, win32Info.monitorInfo.rcMonitor.bottom - win32Info.monitorInfo.rcMonitor.top),
+                                WorkArea = new Rect(win32Info.monitorInfo.rcWork.left, win32Info.monitorInfo.rcWork.top, win32Info.monitorInfo.rcWork.right - win32Info.monitorInfo.rcWork.left, win32Info.monitorInfo.rcWork.bottom - win32Info.monitorInfo.rcWork.top),
+                                IsPrimary = win32Info.monitorInfo.dwFlags > 0,
+                                Hmon = hMonitor,
+                                DeviceName = win32Info.szDevice.ToString()
+                            };
+                            result.Add(info);
+                        }
+                        return true;
+                    }, IntPtr.Zero);
+            }
             return result;
         }
     }
